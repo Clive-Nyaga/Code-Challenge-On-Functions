88a8f316707d1599caa11f67af18a999f9aae129
Clive Nyaga Kagotho <clive.kagotho@student.moringaschool.com>
2025-06-07 13:27:35 +0300

    Enhances interface for mobileMoneyEstimator.js

--- mobileMoneyEstimator/mobileMoneyEstimator.js
+++ mobileMoneyEstimator/mobileMoneyEstimator.js
@@ -3,15 +3,27 @@ function estimateTransactionFee(amountToSend){
     if (transactionFee < 10) {
         transactionFee = 10;
         totalAmount = amountToSend + transactionFee;
-        console.log(totalAmount)
     }
     else if (transactionFee > 70) {
         transactionFee = 70;
         totalAmount = amountToSend + transactionFee;
-        console.log(totalAmount)
     }
     else {
         totalAmount = amountToSend + transactionFee;
-        console.log(totalAmount)
     }
+
+    console.log(`Sending KES ${amountToSend}:
+Calculated Transaction Fee: KES ${transactionFee}
+Total amount to be debited: KES ${totalAmount}
+
+Send Money Securely!`)
+}
+
+let input = prompt("Unatuma Ngapi? (KES): ");
+let amount = Number(input);
+
+if (isNaN(amount) || amount <= 0) {
+  console.log("Please enter a valid amount greater than 0.");
+} else {
+  estimateTransactionFee(amount);
 }
